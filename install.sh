c2f15c8f2953f9baa8ea90a4ccb550fd7f03457e
Leith Johnson <leith.johnson@gmail.com>
2025-08-10 11:02:53 -0600

    Cleaning up lighttpd configuration.

--- install.sh
+++ install.sh
@@ -2,3 +2,10 @@
 
 # Not necessarily an install script.   Mostly documentation of what needs to 
 # be done.   Could turn into an install script.
+
+#sudo /usr/sbin/lighty-enable-mod accesslog
+#sudo /usr/sbin/lighty-disable-mod accesslog
+
+sudo /usr/sbin/lighty-enable-mod fastcgi
+
+sudo ln -s /home/leith/bmon/config/lighttpd.conf /etc/lighttpd
